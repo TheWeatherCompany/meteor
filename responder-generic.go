e0b6415c42bf97762088ee76ce3ee3834e901791
Travis Smith <travis.smith@weather.com>
2018-01-05 17:19:10 -0500

    remove unused code

--- responder-generic.go
+++ responder-generic.go
@@ -13,28 +13,3 @@ func GenericResponder() *genericResponder {
 type genericResponder struct {
 	responder
 }
-
-//// Respond creates the proper response object.
-//func (r *genericResponder) Respond(req *http.Request, resp *http.Response, err error) Responder {
-//	r.Request = req
-//	r.Response = resp
-//	r.Error = err
-//
-//	return r
-//}
-//
-//// DoResponse does the actual response.
-//func (r *genericResponder) DoResponse() (*http.Response, error) {
-//	return r.Response, r.Error
-//}
-//
-//// GetSuccess gets the success struct.
-//func (r *genericResponder) GetSuccess() interface{} {
-//	return r.Success
-//}
-//
-//// GetFailure gets the failure struct.
-//func (r *genericResponder) GetFailure() interface{} {
-//	return r.Failure
-//}
-
